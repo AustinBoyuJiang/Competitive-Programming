558bd43d9062b5b22ef77dbae79927ec8a333a55
AustinBoyuJiang <austinjiangboyu@gmail.com>
2022-10-30 21:39:27 -0700

    USACO gold

---
+++ Competition/USACO/2020/February/Gold/委托.cpp
@@ -0,0 +1,149 @@
+/*
+ * Author: Austin Jiang
+ * Date: 10/30/2022 8:39:51 PM
+ * Problem:
+ * Description:
+*/
+
+#pragma GCC optimize(2)
+#pragma GCC optimize(3)
+#include<bits/stdc++.h>
+//#define int long long
+#define pb push_back
+#define fir first
+#define sec second
+#define endl '\n'
+#define lb lower_bound
+#define ub upper_bound
+#define all(v) v.begin(),v.end()
+#define PQ priority_queue
+#define random(a,b) rand()%(b-a+1)+a
+#define rep(i,x,y) for(int i=(x);i<=(y);i++)
+#define per(i,x,y) for(int i=(x);i>=(y);i--)
+using namespace std;
+using ll = long long;
+using PI = pair<int,int>;
+using VI = vector<int>;
+using VPI = vector<PI>;
+
+namespace fast_io{
+	inline int read() {int x=0; bool f=0; char ch=0; while(!isdigit(ch)) f=ch=='-',ch=getchar(); while(isdigit(ch)) x=x*10+ch-'0',ch=getchar(); return f?-x:x;}
+	inline ll readLL() {ll x=0; bool f=0; char ch=0; while(!isdigit(ch)) f=ch=='-',ch=getchar(); while(isdigit(ch)) x=x*10+ch-'0',ch=getchar(); return f?-x:x;}
+	inline int read(int &x) {return x=read();}
+    template<typename T> inline void write(T x) {if(x<0) x=-x,putchar('-'); if(x>9) write(x/10); putchar(x%10+'0');}
+	template<typename T> inline void write(T x,char let) {write(x),putchar(let);}
+} using namespace fast_io;
+
+/* Common constants, functions, and data structures */
+const int INF = 0x3f3f3f3f;
+const ll LLINF = 0x3f3f3f3f3f3f3f3f;
+const int MOD = 1e9+7;
+const int dir[8][2] = {{1,0},{0,1},{0,-1},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1}};
+
+namespace comfun{
+	template<typename T> inline T lowbit(T x){return x&-x;}
+	template<typename T> inline T gcd(T a,T b){return b?gcd(b,a%b):a;}
+	template<typename T> inline T lcm(T a,T b){return a/gcd(a,b)*b;}
+	template<typename T> inline T chkmax(T &a,T b){return a=max(a,b);}
+	template<typename T> inline T chkmin(T &a,T b){return a=min(a,b);}
+	template<typename T> inline T pow(T a,T b){T ans=1;while(b){if(b&1) ans*=a,ans%=MOD;a*=a,a%=MOD;b>>=1;}return ans;}
+	template<typename T> inline T inv(T x){return pow(x,MOD-2);}
+	template<typename T> inline bool is_prime(T x){if(x==1) return false; for(T i=2;i*i<=x;i++) if(x%i==0) return false; return true;}
+} using namespace comfun;
+
+struct fenwick{
+	int sum[(int)1e6+10];
+	void add(int x,int y){ for(int i=x;i<=1e6;i+=lowbit(i)) sum[i]+=y;}
+	int ask(int x,int y){ int res=0; for(int i=y;i>0;i-=lowbit(i)) res+=sum[i];
+	for(int i=x-1;i>0;i-=lowbit(i)) res-=sum[i]; return res;}
+};
+
+struct fenwick_interval{
+	int d[(int)1e6+10][2];
+	void update(int x,int v){for(int i=x;i<=1e6;i+=lowbit(i))d[i][0]+=v,d[i][1]+=v*x;}
+	int query(int x,int k){int ans=0;for(int i=x;i>0;i-=lowbit(i)) ans+=d[i][k];return ans;}
+	int add(int x,int y,int v){update(x,v),update(y+1,-v);}
+	int ask(int x,int y){return (y+1)*query(y,0)-query(y,1)-x*query(x-1,0)+query(x-1,1);}
+};
+
+/* ========================================| Main Program |======================================== */
+
+const int N = 1e6+10;
+int n,ans[N];
+bool res;
+VI e[N];
+
+int dfs(int u,int fa,int x){
+	if(!res) return 0;
+	map<int,int> cnt;
+	for(auto v:e[u]){
+		if(v==fa) continue;
+		int res=dfs(v,u,x);
+		if(res==0) continue;
+		if(cnt[x-res]) cnt[x-res]--;
+		else cnt[res]++;
+	}
+	int tot=0,sum=fa?1:0;
+	for(auto i:cnt){
+		if(i.sec){
+			tot+=i.sec;
+			sum+=i.fir*i.sec;
+		}
+	}
+	if(tot>=2){
+		res=0;
+		return 0;
+	}
+	return sum%x;
+}
+
+bool check(int x){
+	res=1;
+	return dfs(1,0,x)==0&&res;
+}
+
+void solve(int Case){
+	cin>>n;
+	rep(i,1,n-1){
+		int u,v;
+		cin>>u>>v;
+		e[u].pb(v);
+		e[v].pb(u);
+	}
+	for(int i=1;i*i<=n-1;i++){
+		if((n-1)%i) continue;
+		ans[i]=check(i);
+		ans[(n-1)/i]=check((n-1)/i);
+	}
+	rep(i,1,n-1) cout<<ans[i];
+	cout<<endl;
+}
+
+/* ======================================| Main Program End |====================================== */
+
+signed main(){
+	srand(time(0));
+    //int size(512<<20);  //512M
+    //__asm__("movq %0, %%rsp\n"::"r"((char*)malloc(size)+size));
+	//cin.tie(nullptr)->sync_with_stdio(false);
+	//freopen("in.txt","r",stdin);
+	//freopen("stdout.txt","w",stdout);
+	int CASE=1;
+	//cin>>CASE;
+	rep(Case,1,CASE) solve(Case);
+    //exit(0);
+	//system("fc stdout.txt out.txt");
+	return 0;
+}
+
+/* stuff you should look for
+	* read questions at least 3 times!!!
+    * think more and then code!!!
+    * partial points are GOD.
+	* remember to initialize variables
+    * don't stuck on one question for two long (like 30-45 min)
+    * Debug: (a) read your code once, check overflow, check edge case
+    * Debug: (b) create your own test case
+    * Debug: (c) duipai
+*/
+
---
+++ Competition/USACO/2020/January/Gold/跳板.cpp
@@ -0,0 +1,165 @@
+/*
+ * Author: Austin Jiang
+ * Date: 10/27/2022 3:46:12 PM
+ * Problem:
+ * Description:
+*/
+
+//#pragma GCC optimize(2)
+//#pragma GCC optimize(3)
+#include<bits/stdc++.h>
+//#define int long long
+#define pb push_back
+#define fir first
+#define sec second
+#define endl '\n'
+#define lb lower_bound
+#define ub upper_bound
+#define all(v) v.begin(),v.end()
+#define PQ priority_queue
+#define random(a,b) rand()%(b-a+1)+a
+#define rep(i,x,y) for(int i=(x);i<=(y);i++)
+#define per(i,x,y) for(int i=(x);i>=(y);i--)
+using namespace std;
+using ll = long long;
+using PI = pair<int,int>;
+using VI = vector<int>;
+using VPI = vector<PI>;
+
+namespace fast_io{
+	inline int read() {int x=0; bool f=0; char ch=0; while(!isdigit(ch)) f=ch=='-',ch=getchar(); while(isdigit(ch)) x=x*10+ch-'0',ch=getchar(); return f?-x:x;}
+	inline ll readLL() {ll x=0; bool f=0; char ch=0; while(!isdigit(ch)) f=ch=='-',ch=getchar(); while(isdigit(ch)) x=x*10+ch-'0',ch=getchar(); return f?-x:x;}
+	inline int read(int &x) {return x=read();}
+    template<typename T> inline void write(T x) {if(x<0) x=-x,putchar('-'); if(x>9) write(x/10); putchar(x%10+'0');}
+	template<typename T> inline void write(T x,char let) {write(x),putchar(let);}
+} using namespace fast_io;
+
+/* Common constants, functions, and data structures */
+const int INF = 0x3f3f3f3f;
+const ll LLINF = 0x3f3f3f3f3f3f3f3f;
+const int MOD = 1e9+7;
+const int dir[8][2] = {{1,0},{0,1},{0,-1},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1}};
+
+namespace comfun{
+	template<typename T> inline T lowbit(T x){return x&-x;}
+	template<typename T> inline T gcd(T a,T b){return b?gcd(b,a%b):a;}
+	template<typename T> inline T lcm(T a,T b){return a/gcd(a,b)*b;}
+	template<typename T> inline T chkmax(T &a,T b){return a=max(a,b);}
+	template<typename T> inline T chkmin(T &a,T b){return a=min(a,b);}
+	template<typename T> inline T pow(T a,T b){T ans=1;while(b){if(b&1) ans*=a,ans%=MOD;a*=a,a%=MOD;b>>=1;}return ans;}
+	template<typename T> inline T inv(T x){return pow(x,MOD-2);}
+	template<typename T> inline bool is_prime(T x){if(x==1) return false; for(T i=2;i*i<=x;i++) if(x%i==0) return false; return true;}
+} using namespace comfun;
+
+struct fenwick{
+	int sum[(int)1e6+10];
+	void add(int x,int y){ for(int i=x;i<=1e6;i+=lowbit(i)) sum[i]+=y;}
+	int ask(int x,int y){ int res=0; for(int i=y;i>0;i-=lowbit(i)) res+=sum[i];
+	for(int i=x-1;i>0;i-=lowbit(i)) res-=sum[i]; return res;}
+};
+
+struct fenwick_interval{
+	int d[(int)1e6+10][2];
+	void update(int x,int v){for(int i=x;i<=1e6;i+=lowbit(i))d[i][0]+=v,d[i][1]+=v*x;}
+	int query(int x,int k){int ans=0;for(int i=x;i>0;i-=lowbit(i)) ans+=d[i][k];return ans;}
+	int add(int x,int y,int v){update(x,v),update(y+1,-v);}
+	int ask(int x,int y){return (y+1)*query(y,0)-query(y,1)-x*query(x-1,0)+query(x-1,1);}
+};
+
+/* ========================================| Main Program |======================================== */
+
+const int N = 4e5+10;
+int n,m,ans,dp[N],st[N<<2];
+set<int> s;
+VI num;
+
+struct portal{
+	int x,y,id,tp,dis;
+} p[N];
+
+bool cmp(portal a,portal b){
+	if(a.y==b.y) return a.x<b.x;
+	return a.y<b.y;
+}
+
+void update(int rt,int l,int r,int x,int y){
+	if(l==r){
+		st[rt]=y;
+		return;
+	}
+	int mid=l+r>>1;
+	if(x<=mid) update(rt<<1,l,mid,x,y);
+	else update(rt<<1|1,mid+1,r,x,y);
+	st[rt]=max(st[rt<<1],st[rt<<1|1]);
+}
+
+int ask(int rt,int l,int r,int x,int y){
+	if(l==x&&r==y) return st[rt];
+	int mid=l+r>>1;
+	if(y<=mid) return ask(rt<<1,l,mid,x,y);
+	else if(x>mid) return ask(rt<<1|1,mid+1,r,x,y);
+	return max(ask(rt<<1,l,mid,x,mid),ask(rt<<1|1,mid+1,r,mid+1,y));
+}
+
+void solve(int Case){
+	cin>>n>>m;
+	rep(i,1,m){
+		cin>>p[i].x>>p[i].y;
+		cin>>p[i+m].x>>p[i+m].y;
+		
+		p[i].id=p[i+m].id=i;
+		p[i].dis=p[i+m].y+p[i+m].x-p[i].x-p[i].y;
+		p[i+m].tp=1;
+		
+		s.insert(p[i].x);
+		s.insert(p[i].y);
+		s.insert(p[i+m].x);
+		s.insert(p[i+m].y);
+	}
+	for(auto x:s) num.pb(x);
+	rep(i,1,m*2){
+		p[i].x=lb(all(num),p[i].x)-num.begin()+1;
+		p[i].y=lb(all(num),p[i].y)-num.begin()+1;
+	}
+	sort(p+1,p+m*2+1,cmp);
+	rep(i,1,m*2){
+		if(p[i].tp==0){
+			dp[p[i].id]=ask(1,1,m*4,1,p[i].x)+p[i].dis;
+			chkmax(ans,dp[p[i].id]);
+		}
+		else{
+			int x=max(ask(1,1,m*4,p[i].x,p[i].x),dp[p[i].id]);
+			update(1,1,m*4,p[i].x,x);
+		}
+	}
+	cout<<n*2-ans<<endl;
+}
+
+/* ======================================| Main Program End |====================================== */
+
+signed main(){
+	srand(time(0));
+    //int size(512<<20);  //512M
+    //__asm__("movq %0, %%rsp\n"::"r"((char*)malloc(size)+size));
+	//cin.tie(nullptr)->sync_with_stdio(false);
+	//freopen("in.txt","r",stdin);
+	//freopen("stdout.txt","w",stdout);
+	int CASE=1;
+	//cin>>CASE;
+	rep(Case,1,CASE) solve(Case);
+    //exit(0);
+	//system("fc stdout.txt out.txt");
+	return 0;
+}
+
+/* stuff you should look for
+	* read questions at least 3 times!!!
+    * think more and then code!!!
+    * partial points are GOD.
+	* remember to initialize variables
+    * don't stuck on one question for two long (like 30-45 min)
+    * Debug: (a) read your code once, check overflow, check edge case
+    * Debug: (b) create your own test case
+    * Debug: (c) duipai
+*/
+
