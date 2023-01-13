051f6e4d99b45d98b0b590f9268089ae36723685
AustinBoyuJiang <austinjiangboyu@gmail.com>
2023-01-13 07:31:47 -0800

    Update

---
+++ Competitions/DMOJ/Mock CCC/Mock CCC '23 1 Senior/Richard The Penguin.cpp
@@ -0,0 +1,228 @@
+/*
+ * Author: Austin Jiang
+ * Date: 1/11/2023 9:12:55 PM
+ * Problem: Richard The Penguin
+ * Source:
+ * Description:
+*/
+
+/* Configuration */
+//#define MULTICASES
+//#define LOCAL
+//#define READLOCAL
+//#define FILESCOMP
+//#define SETMEM
+#define FASTIO
+#define OPTIMIZE
+#define INTTOLL
+
+#ifdef OPTIMIZE
+#pragma GCC optimize(2)
+#pragma GCC optimize(3)
+#endif
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifdef INTTOLL
+#define int long long
+#endif
+
+#define rep(i,x,y) for(int i=(x);i<=(y);i++)
+#define per(i,x,y) for(int i=(x);i>=(y);i--)
+#define endl '\n'
+
+/* Pair */
+#define fir first
+#define sec second
+
+/* Segment Tree */
+#define lc (rt << 1)
+#define rc (rt << 1 | 1)
+
+/* STL Data Structures */
+#define lb lower_bound
+#define ub upper_bound
+#define ins insert
+#define eb emplace_back
+#define ef emplace_front
+#define pb push_back
+#define pf push_front
+#define all(v) v.begin(), v.end()
+
+/* Random */
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define random(a,b) rng()%(b-a+1)+a
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using PI = pair<int,int>;
+using VI = vector<int>;
+using VPI = vector<PI>;
+template <typename T> using VEC = vector<T>;
+template <typename T> using US = unordered_set<T>;
+template <typename T> using MS = multiset<T>;
+template <typename T1, typename T2> using UM = unordered_map<T1,T2>;
+template <typename T> using PQ = priority_queue<T>;
+template <typename T> using PQG = priority_queue<T,vector<T>,greater<T>>;
+
+namespace fastIO{
+	inline int read() {int x=0; bool f=0; char ch=0; while(!isdigit(ch)) f=ch=='-',ch=getchar(); while(isdigit(ch)) x=x*10+ch-'0',ch=getchar(); return f?-x:x;}
+	inline ll readLL() {ll x=0; bool f=0; char ch=0; while(!isdigit(ch)) f=ch=='-',ch=getchar(); while(isdigit(ch)) x=x*10+ch-'0',ch=getchar(); return f?-x:x;}
+	inline int read(int &x) {return x=read();}
+    template<typename T> inline void write(T x) {if(x<0) x=-x,putchar('-'); if(x>9) write(x/10); putchar(x%10+'0');}
+	template<typename T> inline void write(T x,char let) {write(x),putchar(let);}
+} using namespace fastIO;
+
+void SETUP(){
+	#ifdef FASTIO
+	cin.tie(nullptr)->sync_with_stdio(false);
+	#endif
+	#ifdef READLOCAL
+	freopen("in.txt","r",stdin);
+	freopen("stdout.txt","w",stdout);
+	#endif
+	srand(time(0));
+}
+
+/* Commonly used constants variables, functions, and data structures */
+const ll LLINF = 0x3f3f3f3f3f3f3f3f;
+#ifndef int
+const int INF = 0x3f3f3f3f;
+#else
+const ll INF = LLINF;
+#endif
+const int MOD = 998244353;
+const int dir[8][2] = {{1,0},{0,1},{0,-1},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1}};
+const US<char> vowel = {'a','e','i','o','u'};
+
+namespace comfun{
+	template<typename T> inline T lowbit(T x){return x&-x;}
+	template<typename T> inline T gcd(T a,T b){return b?gcd(b,a%b):a;}
+	template<typename T> inline T lcm(T a,T b){return a/gcd(a,b)*b;}
+	template<typename T> inline T chkmax(T &a,T b){return a=max(a,b);}
+	template<typename T> inline T chkmin(T &a,T b){return a=min(a,b);}
+	template<typename T> inline T qpow(T a,T b){
+	T ans=1;while(b){if(b&1) ans*=a,ans%=MOD;a*=a,a%=MOD;b>>=1;}return ans;}
+	template<typename T> inline T inv(T x){return qpow(x,MOD-2);}
+	template<typename T> inline bool is_prime(T x){
+	if(x==1) return false; for(T i=2;i*i<=x;i++) if(x%i==0) return false; return true;}
+} using namespace comfun;
+
+struct fenwick{
+	int sum[(int)1e6+10];
+	void add(int x,int y){ for(int i=x;i<=1e6;i+=lowbit(i)) sum[i]+=y;}
+	int ask(int x,int y){ int res=0; for(int i=y;i>0;i-=lowbit(i)) res+=sum[i];
+	for(int i=x-1;i>0;i-=lowbit(i)) res-=sum[i]; return res;}
+};
+
+struct interval_fenwick{
+	int d[(int)1e6+10][2];
+	void update(int x,int v){for(int i=x;i<=1e6;i+=lowbit(i))d[i][0]+=v,d[i][1]+=v*x;}
+	int query(int x,int k){int ans=0;for(int i=x;i>0;i-=lowbit(i)) ans+=d[i][k];return ans;}
+	void add(int x,int y,int v){update(x,v),update(y+1,-v);}
+	int ask(int x,int y){return (y+1)*query(y,0)-query(y,1)-x*query(x-1,0)+query(x-1,1);}
+};
+
+/* ========================================| Main Program |======================================== */
+
+const int N = 5e3+10;
+int n,k,ans,dp[N],f[N][N];
+//dp[i]表示前i置的组合数 
+
+void SOLVE(int Case){
+	cin>>n>>k;
+	if(k<=2){
+		if(n==2){
+			cout<<1<<endl;
+			return;
+		}
+		if(k==1) cout<<0<<endl;
+		else cout<<2<<endl;
+		return;
+	}
+	if(k==n-1){
+		int ans=1;
+		rep(i,1,n-2){
+			ans*=3;
+			ans%=MOD;
+		}
+		cout<<ans<<endl;
+		return;
+	}
+	
+	dp[0]=1;
+	rep(i,1,n-2){
+		rep(j,max(0ll,i-k),i-1){
+			dp[i]+=dp[j];
+			dp[i]%=MOD;
+		}
+		if(i>=k) dp[i]=(dp[i]-dp[i-k]+MOD)%MOD;
+	}
+	rep(i,1,n-2){
+		if(i<=k) rep(j,0,i-1) f[i][j]=dp[j];
+//		if(i==3) cout<<f[3][1]<<endl;
+		rep(j,max(1ll,i-k),i-1){
+			rep(lst,0,i-1) rep(jlst,max(0ll,lst-k),lst){
+				if(lst!=j){
+//					if(i==3&&lst==1&&f[j][jlst]){
+//						cout<<j<<" "<<jlst<<endl;
+//					}
+					if(j>lst&&lst!=jlst) continue;
+					f[i][lst]+=f[j][jlst];
+					f[i][lst]%=MOD;
+				}
+			}
+		}
+	}
+	rep(i,n-k-1,n-2){
+		rep(j,n-k-1,i-1){
+//			if(f[i][j]) cout<<i<<"-"<<j<<" "<<f[i][j]<<endl;
+			ans+=f[i][j];
+			ans%=MOD;
+		}
+	}
+//	cout<<f[3][1]<<endl;
+	cout<<ans*2%MOD<<endl;
+}
+
+/* =====================================| End of Main Program |===================================== */
+
+signed main(){
+	#ifdef SETMEM
+    int size(512<<20);  //512MB
+    __asm__("movq %0, %%rsp\n"::"r"((char*)malloc(size)+size));
+	#endif
+	#ifndef FILESCOMP
+	SETUP();
+	int CASE=1;
+	#ifdef MULTICASES
+	cin>>CASE;
+	#endif
+	rep(i,1,CASE){
+		#ifdef LOCAL
+		cout<<"Case #"<<i<<": "<<endl;
+		#endif
+		SOLVE(i);
+	}
+	#else
+	system("fc stdout.txt out.txt");
+	#endif
+	#ifdef SETMEM
+    exit(0);
+    #endif
+	return 0;
+}
+
+/* stuff you should look for
+	* read questions at least 3 times!!!
+    * think more and then code!!!
+    * partial points are GOD.
+	* remember to initialize variables
+    * don't stuck on one question for two long (like 30-45 min)
+    * Debug: (a) read your code once, check overflow and edge case
+    * Debug: (b) create your own test case
+    * Debug: (c) duipai
+*/
+
---
+++ Competitions/DMOJ/Mock CCC/Mock CCC '23 1 Senior/The Obligatory Data Structures Problem.cpp
@@ -0,0 +1,214 @@
+/*
+ * Author: Austin Jiang
+ * Date: 1/11/2023 10:00:23 PM
+ * Problem:
+ * Source:
+ * Description:
+*/
+
+/* Configuration */
+//#define MULTICASES
+//#define LOCAL
+//#define READLOCAL
+//#define FILESCOMP
+//#define SETMEM
+#define FASTIO
+//#define OPTIMIZE
+#define INTTOLL
+
+#ifdef OPTIMIZE
+#pragma GCC optimize(2)
+#pragma GCC optimize(3)
+#endif
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifdef INTTOLL
+#define int long long
+#endif
+
+#define rep(i,x,y) for(int i=(x);i<=(y);i++)
+#define per(i,x,y) for(int i=(x);i>=(y);i--)
+#define endl '\n'
+
+/* Pair */
+#define fir first
+#define sec second
+
+/* Segment Tree */
+#define lc (rt << 1)
+#define rc (rt << 1 | 1)
+
+/* STL Data Structures */
+#define lb lower_bound
+#define ub upper_bound
+#define ins insert
+#define eb emplace_back
+#define ef emplace_front
+#define pb push_back
+#define pf push_front
+#define all(v) v.begin(), v.end()
+
+/* Random */
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define random(a,b) rng()%(b-a+1)+a
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using PI = pair<int,int>;
+using VI = vector<int>;
+using VPI = vector<PI>;
+template <typename T> using VEC = vector<T>;
+template <typename T> using US = unordered_set<T>;
+template <typename T> using MS = multiset<T>;
+template <typename T1, typename T2> using UM = unordered_map<T1,T2>;
+template <typename T> using PQ = priority_queue<T>;
+template <typename T> using PQG = priority_queue<T,vector<T>,greater<T>>;
+
+namespace fastIO{
+	inline int read() {int x=0; bool f=0; char ch=0; while(!isdigit(ch)) f=ch=='-',ch=getchar(); while(isdigit(ch)) x=x*10+ch-'0',ch=getchar(); return f?-x:x;}
+	inline ll readLL() {ll x=0; bool f=0; char ch=0; while(!isdigit(ch)) f=ch=='-',ch=getchar(); while(isdigit(ch)) x=x*10+ch-'0',ch=getchar(); return f?-x:x;}
+	inline int read(int &x) {return x=read();}
+    template<typename T> inline void write(T x) {if(x<0) x=-x,putchar('-'); if(x>9) write(x/10); putchar(x%10+'0');}
+	template<typename T> inline void write(T x,char let) {write(x),putchar(let);}
+} using namespace fastIO;
+
+void SETUP(){
+	#ifdef FASTIO
+	cin.tie(nullptr)->sync_with_stdio(false);
+	#endif
+	#ifdef READLOCAL
+	freopen("in.txt","r",stdin);
+	freopen("stdout.txt","w",stdout);
+	#endif
+	srand(time(0));
+}
+
+/* Commonly used constants variables, functions, and data structures */
+const ll LLINF = 0x3f3f3f3f3f3f3f3f;
+#ifndef int
+const int INF = 0x3f3f3f3f;
+#else
+const ll INF = LLINF;
+#endif
+const int MOD = 998244353;
+const int dir[8][2] = {{1,0},{0,1},{0,-1},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1}};
+const US<char> vowel = {'a','e','i','o','u'};
+
+namespace comfun{
+	template<typename T> inline T lowbit(T x){return x&-x;}
+	template<typename T> inline T gcd(T a,T b){return b?gcd(b,a%b):a;}
+	template<typename T> inline T lcm(T a,T b){return a/gcd(a,b)*b;}
+	template<typename T> inline T chkmax(T &a,T b){return a=max(a,b);}
+	template<typename T> inline T chkmin(T &a,T b){return a=min(a,b);}
+	template<typename T> inline T qpow(T a,T b){
+	T ans=1;while(b){if(b&1) ans*=a,ans%=MOD;a*=a,a%=MOD;b>>=1;}return ans;}
+	template<typename T> inline T inv(T x){return qpow(x,MOD-2);}
+	template<typename T> inline bool is_prime(T x){
+	if(x==1) return false; for(T i=2;i*i<=x;i++) if(x%i==0) return false; return true;}
+} using namespace comfun;
+
+struct fenwick{
+	int sum[(int)1e6+10];
+	void add(int x,int y){ for(int i=x;i<=1e6;i+=lowbit(i)) sum[i]+=y;}
+	int ask(int x,int y){ int res=0; for(int i=y;i>0;i-=lowbit(i)) res+=sum[i];
+	for(int i=x-1;i>0;i-=lowbit(i)) res-=sum[i]; return res;}
+};
+
+struct interval_fenwick{
+	int d[(int)1e6+10][2];
+	void update(int x,int v){for(int i=x;i<=1e6;i+=lowbit(i))d[i][0]+=v,d[i][1]+=v*x;}
+	int query(int x,int k){int ans=0;for(int i=x;i>0;i-=lowbit(i)) ans+=d[i][k];return ans;}
+	void add(int x,int y,int v){update(x,v),update(y+1,-v);}
+	int ask(int x,int y){return (y+1)*query(y,0)-query(y,1)-x*query(x-1,0)+query(x-1,1);}
+};
+
+/* ========================================| Main Program |======================================== */
+
+int A, B, C = ~(1<<31), M = (1<<16)-1, last;
+int rnd(int last) {
+  A = (36969 + (last >> 3)) * (A & M) + (A >> 16);
+  B = (18000 + (last >> 3)) * (B & M) + (B >> 16);
+  return (C & ((A << 16) + B)) % 1000000000;
+}
+
+const int N = 2e6+10;
+int n,m,ans,a[N],b[N];
+
+struct segment_tree{
+	int lazy[N<<2]
+	
+	void update(int rt,int l=1,int r=n,int x,int y,int v){
+		
+	}
+	
+	int query(int rt=1,int l=1,int r=n,int x,int y){
+		if(l)
+	}
+} st;
+
+void SOLVE(int Case){
+	cin>>n>>m>>A>>B;
+	rep(i,1,n) cin>>a[i];
+	rep(i,1,n) cin>>b[i];
+	rep(i,1,m){
+		int l=rnd(last)%n+1;
+		int r=rnd(last)%n+1;
+		int x=rnd(last)+1;
+		if(l>r) swap(l,r);
+		if((l+r+x)&1){
+			//Update
+			
+		}
+		else{
+			//Query
+			
+			last=res;
+			ans+=i*res;
+			ans%=MOD;
+		}
+	}
+	cout<<ans<<endl;
+}
+
+/* =====================================| End of Main Program |===================================== */
+
+signed main(){
+	#ifdef SETMEM
+    int size(512<<20);  //512MB
+    __asm__("movq %0, %%rsp\n"::"r"((char*)malloc(size)+size));
+	#endif
+	#ifndef FILESCOMP
+	SETUP();
+	int CASE=1;
+	#ifdef MULTICASES
+	cin>>CASE;
+	#endif
+	rep(i,1,CASE){
+		#ifdef LOCAL
+		cout<<"Case #"<<i<<": "<<endl;
+		#endif
+		SOLVE(i);
+	}
+	#else
+	system("fc stdout.txt out.txt");
+	#endif
+	#ifdef SETMEM
+    exit(0);
+    #endif
+	return 0;
+}
+
+/* stuff you should look for
+	* read questions at least 3 times!!!
+    * think more and then code!!!
+    * partial points are GOD.
+	* remember to initialize variables
+    * don't stuck on one question for two long (like 30-45 min)
+    * Debug: (a) read your code once, check overflow and edge case
+    * Debug: (b) create your own test case
+    * Debug: (c) duipai
+*/
+
