4afeacbf1a5af5ce078b95838348f7ca15250caf
Game Development Club WVSS <austinjiangboyu@gmail.com>
2024-01-30 16:07:49 -0800

    Update

---
+++ Competitions/USACO/2024/January/Gold/a.cpp
@@ -0,0 +1,257 @@
+/*
+ * Author: Austin Jiang
+ * Date: 1/28/2024 7:01:47 PM
+ * Problem:
+ * Source:
+ * Description:
+*/
+
+/* Configuration */
+//#define MULTICASES
+//#define LOCAL
+//#define READLOCAL
+//#define FILESCOMP
+//#define SETMEM
+#define FASTIO
+//#define NDEBUG
+#define OPTIMIZE
+#define INTTOLL
+
+#ifdef OPTIMIZE
+#pragma GCC optimize(2)
+#pragma GCC optimize(3)
+#endif
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifdef INTTOLL
+#define int long long
+#endif
+
+#define rep(i,x,y) for(int i=(x);i<=(y);i++)
+#define per(i,x,y) for(int i=(x);i>=(y);i--)
+#define endl '\n'
+
+/* Pair */
+#define fir first
+#define sec second
+
+/* Segment Tree */
+#define lc (rt << 1)
+#define rc (rt << 1 | 1)
+
+/* STL */
+#define lb lower_bound
+#define ub upper_bound
+#define ins insert
+#define eb emplace_back
+#define ef emplace_front
+#define pb push_back
+#define pf push_front
+#define all(v) v.begin(), v.end()
+
+/* Random */
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define random(a,b) rng()%(b-a+1)+a
+
+/* Data type */
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using PI = pair<int,int>;
+using PPI = pair<PI,int>;
+using VI = vector<int>;
+using VPI = vector<PI>;
+template <class T> using Vec = vector<T>;
+template <class T> using PQ = priority_queue<T>;
+template <class T> using PQG = priority_queue<T,vector<T>,greater<T>>;
+
+/* Set up */
+namespace FastIO{
+	inline int read() {int x=0; bool f=0; char ch=0; while(!isdigit(ch)) f=ch=='-',ch=getchar(); while(isdigit(ch)) x=x*10+ch-'0',ch=getchar(); return f?-x:x;}
+	inline ll readLL() {ll x=0; bool f=0; char ch=0; while(!isdigit(ch)) f=ch=='-',ch=getchar(); while(isdigit(ch)) x=x*10+ch-'0',ch=getchar(); return f?-x:x;}
+	inline int read(int &x) {return x=read();}
+    template<class T> inline void write(T x) {if(x<0) x=-x,putchar('-'); if(x>9) write(x/10); putchar(x%10+'0');}
+	template<class T> inline void write(T x,char ch) {write(x),putchar(ch);}
+} using namespace FastIO;
+
+void SETUP(){
+	#ifdef FASTIO
+	cin.tie(nullptr)->sync_with_stdio(false);
+	#endif
+	#ifdef READLOCAL
+	freopen("in.txt","r",stdin);
+	freopen("stdout.txt","w",stdout);
+	#endif
+	srand(time(0));
+}
+
+/* Constants */
+const ll LLINF = 0x3f3f3f3f3f3f3f3f;
+#ifndef int
+const int INF = 0x3f3f3f3f;
+#else
+const ll INF = LLINF;
+#endif
+const int MOD = 1e9+7;
+const int dir[8][2] = {{1,0},{0,1},{0,-1},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1}};
+const unordered_set<char> vowel = {'a','e','i','o','u'};
+
+/* Common functions */
+
+namespace Comfun{
+	template<class T> inline T lowbit(T x){return x&-x;}
+	template<class T> inline T gcd(T a,T b){return b?gcd(b,a%b):a;}
+	template<class T> inline T lcm(T a,T b){return a/gcd(a,b)*b;}
+	template<class T> inline T chkmax(T &a,T b){return a=max(a,b);}
+	template<class T> inline T chkmin(T &a,T b){return a=min(a,b);}
+	template<class T> inline T qpow(T a,T b){T ans=1;
+	while(b){if(b&1)ans*=a,ans%=MOD;a*=a,a%=MOD;b>>=1;}return ans;}
+	template<class T> inline T inv(T x){return qpow(x,MOD-2);}
+	template<class T> inline bool is_prime(T x){
+	if(x==1) return false; for(T i=2;i*i<=x;i++) if(x%i==0) return false;return true;}
+	inline int mex(VI v){VI vis(v.size(),0);for(int x:v) if(x<v.size()) vis[x]=1;
+	int pos=0;while(pos<v.size()&&vis[pos]) pos++;return pos;}
+	template<class T> inline void discrete(T *st,T *ed,T offset=0){ set<T> num(st,ed); Vec<T> pos(all(num));
+	for (T *itr=st;itr!=ed;++itr){*itr=lb(all(pos),*itr)-pos.begin()+offset;}}
+} using namespace Comfun;
+
+/* ========================================| Main Program |======================================== */
+
+const int N = (int)(1e5+10)<<1;
+
+int n,q,d,dist[2][N][20],fa[2][N][20],dd[2]={0,1};
+VI seg[2],flag[2][2],id[2][2];
+
+void check(vector<int>&pos,int u,int &t) {
+    int need = dd[u ^ 1] ^ (t&1) ^ (pos[u]&1);
+    int idx = ub(all(flag[u][need]), pos[u]) - flag[u][need].begin();
+   	int tmp=min(d,flag[u][need][idx] - pos[u]);
+    pos[u]+=tmp;
+    t+=tmp;
+    d-=tmp;
+}
+
+bool find(vector<int>&seg,int x) {
+	return *lb(all(seg),x)==x;
+}
+
+int get(vector<int>&pos,int cur) {
+    int u=-1;
+    if(find(seg[0],pos[0])&&find(seg[1],pos[1])) u=1;
+    else if(find(seg[0],pos[0])) u=1;
+    else u=0;
+    if((cur&1)==0) u^=1;
+    return u;
+}
+
+void solve(vector<int>&pos,int &t) {
+    int u=get(pos,t);
+    check(pos,u,t);
+    u=get(pos,t);
+    check(pos,u,t);
+}
+
+inline void SOLVE(int Case){
+	cin>>n>>q;
+	rep(i,1,n){
+		char t;
+		int c;
+		cin>>t>>c;
+        if(t=='H') seg[1].pb(c);
+        if(t=='V') seg[0].pb(c);
+	}
+    seg[0].pb(2e9);
+	seg[1].pb(2e9+1);
+	seg[0].pb(2e9+1);
+    seg[1].pb(2e9);
+    sort(all(seg[0]));
+    sort(all(seg[1]));
+    rep(o,0,1){
+        rep(i,0,(int)seg[o].size()-1){
+            id[o][seg[o][i]&1].pb(i);
+            flag[o][seg[o][i]&1].pb(seg[o][i]);
+        }
+        rep(i,0,(int)seg[o].size()-1){
+            if(i<(int)seg[o].size() - 1) {
+                int v = 1 ^seg[o][i]%2;
+                int p = ub(flag[o][v].begin(), flag[o][v].end(), seg[o][i])-flag[o][v].begin();
+                dist[o][i][0] = flag[o][v][p] - seg[o][i];
+                fa[o][i][0] = id[o][v][p];
+                continue;
+            }
+            dist[o][i][0] = 0;
+            fa[o][i][0] = i;
+        }
+        rep(k,1,19){
+            rep(i,0,(int)seg[o].size()){
+                int ff = fa[o][i][k - 1];
+                fa[o][i][k] = fa[o][ff][k - 1];
+                dist[o][i][k] = dist[o][i][k - 1] + dist[o][ff][k - 1];
+            }
+        }
+    }
+    for(int i = 1; i <= q; ++i) {
+        int x,y;
+        cin>>x>>y>>d;
+        VI pos{x,y};
+	    int t=1;
+	    solve(pos,t);
+	    if(d!=0){
+		    per(k,19,0){
+		        int p0=lb(all(seg[0]),pos[0])-seg[0].begin();
+		        int p1=lb(all(seg[1]),pos[1])-seg[1].begin();
+		        int tot=dist[0][p0][k]+dist[1][p1][k];
+		        if(tot<=d) {
+		            pos[0]+=dist[0][p0][k];
+		            pos[1]+=dist[1][p1][k];
+		            d-=tot;
+		            t+=tot;
+		        }
+		    }
+		    solve(pos,t);
+		}
+        cout<<pos[0]<<" "<<pos[1]<<endl;
+    }
+}
+
+/* =====================================| End of Main Program |===================================== */
+
+signed main(){
+	#ifdef SETMEM
+    int size(512<<20);  //512MB
+    __asm__("movq %0, %%rsp\n"::"r"((char*)malloc(size)+size));
+	#endif
+	#ifndef FILESCOMP
+	SETUP();
+	int CASE=1;
+	#ifdef MULTICASES
+	cin>>CASE;
+	#endif
+	rep(i,1,CASE){
+		#ifdef LOCAL
+		printf("Case #%d: \n",i);
+		#endif
+		SOLVE(i);
+	}
+	#else
+	system("fc stdout.txt out.txt");
+	#endif
+	#ifdef SETMEM
+    exit(0);
+    #endif
+	return 0;
+}
+
+/* stuff you should look for
+	* read questions at least 3 times!!!
+    * think more and then code!!!
+    * partial points are GOD.
+	* remember to initialize variables
+    * don't stuck on one question for two long (like 30-45 min)
+    * Debug: (a) read your code once, check overflow and edge case
+    * Debug: (b) create your own test case
+    * Debug: (c) Adversarial Testing
+*/
+
